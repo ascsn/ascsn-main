7dce84b404fd5a57edb54b5f3469367af453c557
Kyle Godbey <me@kyle.ee>
2025-02-25 13:24:02 -0500

    New styling

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 import { useRouter } from 'next/router';
-import { DEV_CLIENT_PAGES_MANIFEST } from 'next/dist/shared/lib/constants'
 import customStyles from '../styles/custom.module.css'
 
 export default function Home({ allPostsData }) {
@@ -30,30 +29,69 @@ export default function Home({ allPostsData }) {
           we are committed to providing resources and support to the broader scientific community to
           promote accessibility and lower the barrier to get involved with cutting edge research.
         </p>
-
+        <p>
+          Join us in our mission to advance scientific computing and statistics. Explore our resources,
+          participate in our forums, and connect with like-minded individuals to drive innovation and discovery.
+        </p>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>ASCSN Resources | </h2>
-        <Link href='https://forum.ascsn.net'>Forum</Link> | <Link href='https://github.com/ascsn'>GitHub</Link> | <Link href='https://www.youtube.com/@ASCSN-channel'>YouTube</Link>
-        {/* if (window.location.href.indexOf("franky") > -1) {
-      alert("your url contains the name franky");
-    } */}
-        <br />
-        <Link href='https://dr.ascsn.net'>Dimensionality Reduction Book</Link> | <Link href='https://qc.ascsn.net'>Quantum Computing Book</Link>
-        {/* <ul className={utilStyles.list}>
+        <h2 className={utilStyles.headingLg}>Community Platforms</h2>
+        <ul className={utilStyles.list}>
           <li className={utilStyles.listItem}>
-            <Link href='https://forum.ascsn.net'>Forum</Link>
+            <Link href='https://forum.ascsn.net'>
+              <span className={customStyles.iconWrapper}>
+                <i className="fa-regular fa-comments"></i>
+                <span>Forum</span>
+              </span>
+            </Link>
           </li>
           <li className={utilStyles.listItem}>
-            <Link href='https://github.com/ascsn'>GitHub</Link>
+            <Link href='https://github.com/ascsn'>
+              <span className={customStyles.iconWrapper}>
+                <i className="fab fa-github"></i>
+                <span>GitHub</span>
+              </span>
+            </Link>
           </li>
-        </ul> */}
-        <h2 className={utilStyles.headingLg}>Blog |</h2>
+          <li className={utilStyles.listItem}>
+            <Link href='https://www.youtube.com/@ASCSN-channel'>
+              <span className={customStyles.iconWrapper}>
+                <i className="fab fa-youtube"></i>
+                <span>YouTube</span>
+              </span>
+            </Link>
+          </li>
+        </ul>
+        <h2 className={utilStyles.headingLg}>Educational Resources</h2>
+        <ul className={utilStyles.list}>
+          <li className={utilStyles.listItem}>
+            <Link href='https://dr.ascsn.net'>
+              <span className={customStyles.iconWrapper}>
+                <i className="fa-solid fa-chart-line"></i>
+                <span>Dimensionality Reduction Book</span>
+              </span>
+            </Link>
+          </li>
+          <li className={utilStyles.listItem}>
+            <Link href='https://qc.ascsn.net'>
+              <span className={customStyles.iconWrapper}>
+                <i className="fa-solid fa-atom"></i>
+                <span>Quantum Computing Book</span>
+              </span>
+            </Link>
+          </li>
+        </ul>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
+              <Link href={`/posts/${id}`}>
+                <span className={customStyles.iconWrapper}>
+                  <i className="fa-regular fa-file-lines"></i>
+                  <span>{title}</span>
+                </span>
+              </Link>
               <br />
               <small className={utilStyles.lightText}>
                 <Date dateString={date} />
@@ -61,22 +99,28 @@ export default function Home({ allPostsData }) {
             </li>
           ))}
         </ul>
-      </section>
-    </Layout>
+      </section>    </Layout>
   )
 }
 
 function DNP() {
   return (
-      <section className={customStyles.dnpbox}>
-        <h1>Welcome DNP Visitors!</h1>
-        <p>
-          This page is a special landing page for visitors to the 2024 Division of Nuclear Physics
-          meeting. The forum link to the discussion board for the meeting is <a href="https://forum.ascsn.net/t/about-the-lifting-the-shadows-category/309">here</a>.
-          
-        </p>
-      </section>
-
+    <section className={customStyles.dnpbox}>
+      <h1>Welcome DNP Visitors!</h1>
+      <p>
+        This page is a special landing page for visitors to the 2024 Division of Nuclear Physics
+        meeting. The forum link to the discussion board for the meeting is <a href="https://forum.ascsn.net/t/about-the-lifting-the-shadows-category/309">here</a>.
+      </p>
+      <p>
+        We are excited to have you join us and explore the various resources and discussions available. Feel free to participate in the forums, ask questions, and connect with other attendees.
+      </p>
+      <p>
+        Don't forget to check out our <a href="https://ascsn.net/resources">resources page</a> for more information and materials related to the conference.
+      </p>
+      <p>
+        Additionally, you can find the schedule of events and important announcements on our <a href="https://ascsn.net/events">events page</a>.
+      </p>
+    </section>
   )
 }
 
